8e8e720a88f54957e96443b46358ef809583e4ff
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-13 11:34:39 +0100

    Add: Main.jsx + Header Components Import

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Header from '../components/header/header.jsx';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('body')).render(
   <StrictMode>
+
+    <Header/>
+
     
   </StrictMode>,
-)
+);
