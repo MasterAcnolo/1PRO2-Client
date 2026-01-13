c8d59790d214881da048da2d366067265e1d6955
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-13 18:51:52 +0100

    Add: Header Components + Various Tweaks

---
+++ components/button/button.jsx
@@ -0,0 +1,16 @@
+import './button.css';
+
+function Login_Sign_Buttons() {
+    return(
+    <>
+        <div className='buttons'>
+            <button id="login" className='hover-btn'>Se connecter</button>
+            <button id="sign-in" className='hover-btn'>Inscrivez vous gratuitement !</button>
+        </div>
+    </>
+);}
+
+
+export {
+    Login_Sign_Buttons
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from '../components/header/header.jsx';
 
-createRoot(document.getElementById('body')).render(
+// Components
+import Header from '../components/header/header.jsx';
+import Footer from '../components/footer/footer.jsx';
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
 
     <Header/>
-
+    {/* <Footer/> */}
     
   </StrictMode>,
 );
