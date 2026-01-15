f84b56ae06baeab65ed6bb994ed1fe44f3d21c76
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-15 11:10:51 +0100

    Add: Footer Components

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+import './footer.css';
+
+
+function Footer() {
+    return(
+        <>
+        <footer>
+            <div className='container'>
+                <nav>
+                    <Link>Boards</Link>
+                    <Link>Vos Board</Link>
+                    <Link>Créer un Board</Link>
+                </nav>
+                <nav>
+                    <Link>Mon espace</Link>
+                    <Link>Compte</Link>
+                    <Link to="/register">S'inscrire</Link>
+                    <Link to="/login">Connexion</Link>
+                </nav>
+                <nav>
+                    <Link>Légal</Link>
+                    <Link>Vos Données</Link>
+                    <Link>Mentions Légales</Link>
+                    <Link>Nous Contacter</Link>
+                </nav>
+            </div>
+            <p>Tous Droits Réservés © TaskLoader.</p>
+        </footer>
+        </>
+    );
+}
+
+export default Footer;
