b863a9a625fa9c853900e247b1acd8469605512f
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-18 21:28:54 +0100

    Fix: Change File Structure

---
+++ src/helpers/dropdown/dropdown.jsx
@@ -0,0 +1,22 @@
+import './dropdown.css';
+
+import { useState } from 'react';
+
+export default function DropDown() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return(
+    <>
+       <div className='dropDown'>
+            <img onClick={() => setIsOpen(valeurInitiale => !valeurInitiale)} id="card-option" src='../../../assets/icon/3DotsIcon.png'></img>
+
+            <div className='dropDown-content' style={{display: `${isOpen ? "flex" : "none"}`}}>
+                <p className='edit' id='edit'>Modifier</p>
+                <p className='duplicate' id='duplicate'>Dupliquer</p>
+                <p className='delete' id='delete'>Supprimer</p>
+
+            </div>
+       </div>
+    </>
+);}
