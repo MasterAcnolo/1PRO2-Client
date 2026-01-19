082d087a623697f63c23f67b8c699ae368fda4b6
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-19 21:57:50 +0100

    Fix: Footer Top Link who was not static text.

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -9,18 +9,18 @@ function Footer() {
         <footer>
             <div className='container'>
                 <nav>
-                    <Link>Boards</Link>
+                    <h3>Boards</h3>
                     <Link to="/board">Vos Boards</Link>
                     <Link>Créer un Board</Link>
                 </nav>
                 <nav>
-                    <Link>Mon espace</Link>
+                    <h3>Mon espace</h3>
                     <Link>Compte</Link>
                     <Link to="/register">S'inscrire</Link>
                     <Link to="/login">Connexion</Link>
                 </nav>
                 <nav>
-                    <Link>Légal</Link>
+                    <h3>Légal</h3>
                     <Link>Vos Données</Link>
                     <Link>Mentions Légales</Link>
                     <Link>Nous Contacter</Link>
