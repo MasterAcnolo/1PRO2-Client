fbe53980d98e7f5626f697bfbd2386ec0c631bf0
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-22 16:43:45 +0100

    Feat: Using Hook for checking if user is logged or not

---
+++ script/hooks/hooks.isLogged.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { isLogged } from "../auth";
+
+// Check si jamais l'utilisateur est connecté. Si non, envoie à la page de login
+function useIsLoggedRedirect() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function check() {
+            const user = await isLogged();
+            if (!user) navigate("/login");
+        }
+        check();
+    }, [navigate]);
+}
+
+function useIsLogged() {
+    const [logged, setLogged] = useState(false);
+
+    useEffect(() => {
+        async function check() {
+            const status = await isLogged();
+            setLogged(status);
+        }
+        check();
+    }, []);
+
+    return logged;
+}
+
+export {
+    useIsLoggedRedirect,
+    useIsLogged
+}
