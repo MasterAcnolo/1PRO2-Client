cc0db13a2dfb46ceeb662e7de883eaf8a6e655e6
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-25 12:15:03 +0100

    Add: Hamburger Menu Init

--- src/components/hamburger/hamburger.jsx
+++ src/components/hamburger/hamburger.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 import hamburgerIcon from '../../../assets/icon/hamburger.png';
 import crossIcon from '../../../assets/icon/cross.png';
@@ -26,8 +28,40 @@ function HamburgerMenu() {
 
       {isActive ? 
       <div className='hamburger-overlay'> 
+        <div className='hamburger-backgroundContent'>
+          <div className='hamburger-content'>
 
+            <div className='hamburger-avatar'>
+              <h1>{/*username*/}C Moi !</h1>
+              <img src="../../../assets/default.jpg" alt="Icon d'Avatar" />
+            </div>
 
+            <div className='hamburger-contentText'>
+              <h4>Boards</h4>
+              <p className='hover-btn'><Link>Vos Board</Link></p>
+              <p className='hover-btn'><Link>Créer un Board</Link></p>
+            </div>
+            
+            <span className='horizontal-line'></span>
+
+            <div className='hamburger-contentText'>
+              <h4>Mon Espace</h4>
+              <p className='hover-btn'><Link>S'inscrire</Link></p>
+              <p className='hover-btn'><Link>Connexion</Link></p>
+            </div>
+
+              <span className='horizontal-line'></span>
+
+            {/* A voir si on le garde dans le hamburger */}
+            <div className='hamburger-contentText'>
+              <h4>Légal</h4>
+              <p className='hover-btn'><Link>Vos Données</Link></p>
+              <p className='hover-btn'><Link>Mentions Légales</Link></p>
+            </div>
+
+            <div className='hamburger-disconnect hover-btn'><p><Link>Déconnexion</Link></p></div>
+          </div>
+        </div>
       </div> : ""
       }
 
