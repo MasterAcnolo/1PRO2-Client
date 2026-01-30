2e05f5eda6758296381f7cf55f7d9766c99b29a1
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-30 21:06:53 +0100

    Add: Get Element Services

---
+++ script/services/getElement.js
@@ -0,0 +1,47 @@
+import { getToken } from "../helpers/getToken";
+
+async function getElement(type, ID) {
+    const BASE_URL = "http://localhost:1337/api";
+    const TOKEN = getToken();
+
+    let ENDPOINT = "";
+
+    switch (type) {
+        case "BOARD":
+            ENDPOINT = `/boards/${ID}?populate[columns][populate][0]=cards`; // On populate les colonnes (donc premier niveau,) et pour chaque colonne on va aussi récupérer le second niveau
+            break;
+        case "COLUMN":
+            ENDPOINT = `/columns/${ID}?populate=cards`;
+            break;
+        case "CARD":
+            ENDPOINT = `/cards/${ID}`;
+            break;
+        default:
+            console.log("Invalid Type");
+            return null;
+    }
+
+    const res = await fetch(`${BASE_URL}${ENDPOINT}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${TOKEN}`
+        }
+    });
+
+    let json = null;
+
+    if (res.headers.get("content-type")?.includes("application/json")) {
+        json = await res.json();
+    }
+
+    if (!res.ok) {
+        throw new Error(json?.error?.message || "Erreur inconnue");
+    }
+
+    return json;
+}
+
+export {
+    getElement
+};
