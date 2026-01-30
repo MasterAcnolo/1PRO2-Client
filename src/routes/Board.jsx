2a1df97a5faf6e850367fefa0a9bfc5f4f15520c
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-01-30 21:11:13 +0100

    Add: Board Fetch Init

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -1,18 +1,54 @@
-import TaskCard from '../components/board/column/card/card.jsx';
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
-import {userIsLoggedRedirect} from '../../script/hooks/hooks.isLogged';
+import TaskCard from "../components/board/column/card/card.jsx";
+import { userIsLoggedRedirect } from "../../script/hooks/hooks.isLogged";
+import { getElement } from "../../script/services/getElement";
 
-export default function Board(){
+export default function Board() {
 
     userIsLoggedRedirect();
-    
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [board, setBoard] = useState(null);
+    const [column, setColumn] = useState(null);
+    const [card, setCard] = useState(null);
+
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (!id) {
+            navigate("/board");
+            return;
+        }
+
+        async function fetchBoard() {
+            try {
+                const res = await getElement("BOARD", id);
+                console.log(res.data.name)
+                console.log(res.data)
+                setBoard(res.data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchBoard();
+    }, [id, navigate]);
+
+    if (loading) return <p>Chargement…</p>;
+    if (error) return <p>Erreur : {error}</p>;
+    if (!board || !board.columns) return <p>Aucune colonne</p>;
+
     return (
-    <>
-            <TaskCard 
-            titre="Titre"
-            description="Ceci est une description qui decrit"
-            date="EE"
-            />
-    </>
-    )
+        <> 
+            <h1>{board.name}</h1>
+            <p>{board.description}</p>
+        </>
+    );
 }
