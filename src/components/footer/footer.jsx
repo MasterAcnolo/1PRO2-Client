09bb881de259cef95308925ca188ae568eecd8f6
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-19 13:27:22 +0100

    Fix: Don't show login register if logged (footer)

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,31 +4,41 @@ import { Link } from "react-router-dom";
 // CSS
 import './footer.css';
 
+// Helpers
+import { userIsLogged } from "../../../script/hooks/hooks.isLogged";
+
 
 function Footer() {
-    return(
+    // Le hook retourne true si connecté, false sinon
+    const isLogged = userIsLogged();
+
+    return (
         <>
-        <footer>
-            <div className='container'>
-                <nav>
-                    <h3>Boards</h3>
-                    <Link to="/board">Vos Boards</Link>
-                </nav>
-                <nav>
-                    <h3>Mon espace</h3>
-                    <Link to="/account">Compte</Link>
-                    <Link to="/register">S'inscrire</Link>
-                    <Link to="/login">Connexion</Link>
-                </nav>
-                <nav>
-                    <h3>Légal</h3>
-                    <Link to="/vos-donnees">Vos Données</Link>
-                    <Link to="/plan-du-site">Plan du Site</Link>
-                    <Link to="/mentions-legales">Mentions Légales</Link>
-                </nav>
-            </div>
-            <p>Tous Droits Réservés © TaskLoader.</p>
-        </footer>
+            <footer>
+                <div className='container'>
+                    <nav>
+                        <h3>Boards</h3>
+                        <Link to="/board">Vos Boards</Link>
+                    </nav>
+                    <nav>
+                        <h3>Mon espace</h3>
+                        <Link to="/account">Compte</Link>
+                        {isLogged === false && (
+                            <>
+                                <Link to="/register">S'inscrire</Link>
+                                <Link to="/login">Connexion</Link>
+                            </>
+                        )}
+                    </nav>
+                    <nav>
+                        <h3>Légal</h3>
+                        <Link to="/vos-donnees">Vos Données</Link>
+                        <Link to="/plan-du-site">Plan du Site</Link>
+                        <Link to="/mentions-legales">Mentions Légales</Link>
+                    </nav>
+                </div>
+                <p>Tous Droits Réservés © TaskLoader.</p>
+            </footer>
         </>
     );
 }
