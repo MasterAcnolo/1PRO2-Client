5581892f11690fba5825bfca0aeb6c8afbb29388
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-19 09:49:27 +0100

    Fix: Update API_BASE_URL comment for clarity on usage

--- script/variables.js
+++ script/variables.js
@@ -3,4 +3,4 @@
  * Centralisation des constantes et configurations
  */
 
-export const API_BASE_URL = "http://192.168.1.53:1337/api";
+export const API_BASE_URL = "http://192.168.1.53:1337/api"; // Actuellement mon IP chez moi. Remplacer par LocalHost quand on est en présentiel.
