d6377c89c4cd02e2cde3100daba99b77b42747b7
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-25 17:11:41 +0100

    Fix: Remove Unusual Coms

--- script/variables.js
+++ script/variables.js
@@ -1,6 +1,1 @@
-/**
- * Variables globales de l'application
- * Centralisation des constantes et configurations
- */
-
 export const API_BASE_URL = "http://192.168.1.53:1337/api"; // Actuellement mon IP chez moi. Remplacer par LocalHost quand on est en présentiel.
