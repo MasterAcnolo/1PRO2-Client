c622f7b7edad7aec29c56351582ec140db709a56
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-27 09:31:12 +0100

    Refactor: Enhance board layout , board card preview,

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -318,7 +318,16 @@ export default function Board() {
             <div className="board-header">
                 <h1>{board.name}</h1>
 
-                <button onClick={handleExportBoard} id="export-board">Exporter</button>
+                <button onClick={handleExportBoard} id="export-board" className="exportBtn">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 384 512"
+                        className="exportIcon"
+                    >
+                        <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                    </svg>
+                    <span className="exportBase"></span>
+                </button>
             </div>
 
             <div className="board-container">
