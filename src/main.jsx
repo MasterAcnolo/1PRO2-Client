f084b3535aa7ef5ecf7a15d3438e9f85de67786a
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-02 11:12:13 +0100

    Fix: Remove StrictMode wrapper from main render

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode> {/* TODO: Retirer le "StrictMode" quand on va rendre le projet*/}
     <BrowserRouter>
         <App />
     </BrowserRouter>
-  </StrictMode>
 );
