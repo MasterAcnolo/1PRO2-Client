b297c8ee577ee134f9ff96533e45828c3cc7557f
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-02 16:55:00 +0100

    Refactor: Export BTN

--- src/routes/Board.jsx
+++ src/routes/Board.jsx
@@ -101,13 +101,10 @@ export default function Board() {
             <div className="board-header">
                 <h1>{board.name}</h1>
                 <button id="export-board" className="export-btn" onClick={() => exportBoardAsImage(board?.name || "board")}>
-                    <span className="icon-wrapper">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="export-icon">
-                            <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                        </svg>
-                        <span className="export-tray"></span>
-                    </span>
-                    <span className="export-text">Exporter</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="export-icon">
+                        <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/>
+                    </svg>
+                    Exporter
                 </button>
             </div>
 
